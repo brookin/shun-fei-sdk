9dc123bd9a794597e76451144df2767cc685f0a7
“brookingit config --global user.email “brookin@126.com <brookin@126.com>
2017-12-15 17:38:32 +0800

    initial project

---
+++ src/Brookin/ShunFeiSDK/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Brookin\ShunFeiSDK;
+
+class Test {
+    public static function info() {
+        echo "test info";
+    }
+}
---
+++ tests/Brookin/ShunFeiSDKTest/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Brookin\ShunFeiSDKTest;
+
+class AdTest extends \PHPUnit_Framework_TestCase {
+    public function testHello() {
+        echo 'hello world';
+    }
+}
