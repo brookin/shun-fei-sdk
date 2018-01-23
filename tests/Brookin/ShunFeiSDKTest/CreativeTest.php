982df041235559a94e4c9f3eb5e7dcd1410fcc14
damonpeng <damon@kaifage.com>
2018-01-23 12:04:58 +0800

    shunfei

--- tests/Brookin/ShunFeiSDKTest/CreativeTest.php
+++ tests/Brookin/ShunFeiSDKTest/CreativeTest.php
@@ -11,6 +11,7 @@ use Brookin\ShunFeiSDK\Creative\Creative;
 use Brookin\ShunFeiSDK\Creative\CreativeAddRequest;
 use Brookin\ShunFeiSDK\Creative\CreativeAddResponse;
 use Brookin\ShunFeiSDK\Creative\CreativeRequest;
+use Brookin\ShunFeiSDK\Creative\NativeInfo;
 
 class CreativeTest extends \PHPUnit_Framework_TestCase
 {
@@ -18,11 +19,48 @@ class CreativeTest extends \PHPUnit_Framework_TestCase
     public function testAdd() {
         $request = new CreativeAddRequest();
         $response = new CreativeAddResponse();
-        $creativeRequest = new CreativeRequest();
+        $creative = new CreativeRequest();
 
-        $request->request[] = $creativeRequest;
+//        $creative->setCreativeId(0);
+
+        $creative->setOCreativeId('aff1bb6965ba62ff843cb47b032b019b');
+        $creative->setType(CreativeRequest::Type_IMAGE);
+        $creative->setAdviewType(CreativeRequest::AdviewType_MOBILE);
+        $creative->setAdBidType(CreativeRequest::AdBidType_RTB);
+        $creative->setCreativeUrl('http://cdn.chinatvpay.com/BAAACeZAAAABaZAEnDtctpv_8.png');
+        $creative->setAdm('');  // 动态创意
+
+        $creative->setTargetUrl('http://www.chinatvpay.com');
+//        $creative->setDownloadUrl([]);
+        $creative->setDestinationUrl(['http://www.chinatvpay.com']);
+        $creative->setLandType(CreativeRequest::LandType_BROWSER);
+        $creative->setAppPackage('');
+        $creative->setAppName('');
+
+//        $creative->setTargetTrack([]);
+        $creative->setMonitorUrls(array());
+
+        $creative->setWidth(300);
+        $creative->setHeight(250);
+
+        // @todo native 需要创建规格时录入模版id
+//        if ($creative->getType() === CreativeRequest::Type_NATIVE) {
+//            $native = new NativeTemplateInfo();
+//            $native->setTplId( $creativeSizeEntity->getCreativeSizeAttrsStruct()->getTemplateId() );
+//
+//            $nativeAsset = new NativeAssetInfo();
+//            $nativeAsset->setImage()
+//            $native->setNAssets();
+//        }
+        $creative->setNative(new NativeInfo());
+
+        $creative->setAdvertiserId(326);
+        $creative->setAdxIds([20009]);  // 20009 测试
+
+        $request->setRequest([$creative]);
 
         $service = new Creative();
         $service->add($request, $response);
+
     }
 }
